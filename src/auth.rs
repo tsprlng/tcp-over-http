97409fa862242429e090afdc6519b17ee22acd36
Tom Spurling <tom@tspurling.co.uk>
2025-08-13 16:33:21 +0100

    Randomize auth token

--- src/auth.rs
+++ src/auth.rs
@@ -1,5 +1,11 @@
 use lazy_static::lazy_static;
+use rand::Rng;
+use rand::distr::Alphanumeric;
 
 lazy_static! {
-    pub static ref CORRECT_AUTH_HEADER: String = "Bearer AmRM1SYiJtrl247b1mQp9K".to_owned();
+    pub static ref CORRECT_AUTH_HEADER: String = {
+        let mut rng = rand::rng();
+        let token = rng.sample_iter(Alphanumeric).take(22).collect::<Vec<_>>();
+        format!("Bearer {}", String::from_utf8(token).expect("alphanumeric generator should produce valid utf-8"))
+    };
 }
